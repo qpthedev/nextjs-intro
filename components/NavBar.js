cc84093cc420b5205c5084cccb44f1158f2a153e
qpthedev <qpthedev@gmail.com>
2022-02-08 11:16:28 -0500

    Basic routing with nextjs

---
+++ components/NavBar.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+  return (
+    <nav>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </nav>
+  );
+}
