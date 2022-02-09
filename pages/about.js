e52e93225daa74a2b6392b74c90ea9ef8a8e350f
qpthedev <qpthedev@gmail.com>
2022-02-08 20:21:47 -0500

    Layout pattern setup

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,9 @@
-import NavBar from "../components/NavBar";
+import SEO from "../components/SEO";
 
 export default function About() {
   return (
     <div>
+      <SEO title="About" />
       <h1>About Us</h1>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
-import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
+import SEO from "../components/SEO";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = "";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+      ).json();
+      console.log(data);
+    })();
+  }, []);
+
   return (
     <div>
+      <SEO title="Home" />
       <h1>Hello</h1>
     </div>
   );
