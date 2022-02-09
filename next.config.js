9aa9c4b91c8e89b9d5500489cb00b3442c684d93
qpthedev <qpthedev@gmail.com>
2022-02-08 21:16:08 -0500

    Movie data fetching, API key masking with rewrites

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
-}
+
+  // redirects example
+  async redirects() {
+    return [
+      {
+        source: "/old-site/:path*",
+        destination: "/new-site/:path*",
+        permanent: false,
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `${BASE_URL}/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,49 @@
 import { useEffect, useState } from "react";
 import SEO from "../components/SEO";
 
-const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = "";
-
 export default function Home() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState();
 
   useEffect(() => {
     (async () => {
-      const data = await (
-        await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
-      ).json();
-      console.log(data);
+      const { results } = await (await fetch(`/api/movies`)).json();
+
+      setMovies(results);
     })();
   }, []);
 
   return (
-    <div>
+    <div className="container">
       <SEO title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading</h4>}
+      {movies?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.title}</h4>
+        </div>
+      ))}
+
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
